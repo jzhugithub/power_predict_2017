2b644f7edc4a39a200653531bacd543472eadd34
jzhugithub <mfx106825372@163.com>
2017-05-23 22:54:58 +0800

    consider week

--- run.py
+++ run.py
@@ -8,10 +8,9 @@ class User(object):
     id = 'id'
     date = []
     power = []
-    power_2016_9 = []
     # model data
-    average_power_train = 0.0
-    average_error_vaild = 0.0
+    average_power = 0.0
+    delta_power_in_week = np.zeros(7)
 
     def __init__(self, userid):
         self.id = userid
@@ -22,26 +21,40 @@ class User(object):
         self.date.append(a_date)
         self.power.append(int(a_power))
 
-    def power_without_2016_8(self):
-        return self.power[0:-32]
+    def train(self, array_begin, array_end):
+        self.average_power = self.get_average_power(array_end - 20, array_end)
+        # print('average_power ' + self.id + ': ' + str(self.average_power))
+        self.delta_power_in_week = self.get_delta_power_in_week(array_end - 395, array_end - 335)
+        # print(self.delta_power_in_week)
 
-    def power_2016_8(self):
-        return self.power[-31:-1]
+    def predict(self, array_begin, array_end):
+        power_predict = []
+        for i, value in enumerate(range(array_begin, array_end)):
+            # power_predict.append(self.average_power)
+            power_predict.append(self.average_power + self.delta_power_in_week[value % 7])
+        return power_predict
 
-    def train(self):
-        self.average_power_train = sum(self.power_without_2016_8()[-100:-1]) / 100.0
-        # print('average_power_train ' + self.id + ': ' + str(self.average_power_train))
-
-    def vaild(self):
-        error_vaild = np.array(self.power_2016_8()) - self.average_power_train
-        self.average_error_vaild = sum(abs(error_vaild)) / int(len(self.power_2016_8()))
+    def vaild(self, array_begin, array_end):
+        power_predict = self.predict(array_begin, array_end)
+        error_vaild = np.array(self.power[array_begin:array_end]) - np.array(power_predict)
+        self.average_error_vaild = sum(abs(error_vaild)) / float(len(error_vaild))
         # print('average_error_vaild ' + self.id + ': ' + str(self.average_error_vaild))
 
-    def predict(self):
-        self.power_2016_9 = [int(self.average_power_train) for i in range(30)]
-        # print('predict ' + self.id + ': ')
-        # print(self.date_2016_9)
-        return self.power_2016_9
+    def get_average_power(self, array_begin, array_end):
+        return sum(self.power[array_begin:array_end]) / float(array_end - array_begin)
+
+    def get_delta_power_in_week(self, array_begin, array_end):
+        power_in_week = np.zeros(7)
+        average_power_in_week = np.zeros(7)
+        date_count = np.zeros(7)
+        for index in range(array_begin, array_end):
+            power_in_week[index % 7] = power_in_week[index % 7] + self.power[index]
+            date_count[index % 7] = date_count[index % 7] + 1
+        for index in range(7):
+            average_power_in_week[index] = power_in_week[index] / float(date_count[index])
+        delta_power_in_week = average_power_in_week - average_power_in_week.sum() / 7.0
+
+        return delta_power_in_week
 
 
 def import_date(csv_name):
@@ -108,6 +121,13 @@ def show_figure(show_index, user, sum=False):
             plt.plot(range(len(user[temp_show_index].date)), user[temp_show_index].power, 'r')
             plt.show()
 
+def show_result(user, power_2016_9_total):
+    user_power_total = np.zeros(len(user[0].power))
+    for temp_show_index in range(len(user)):
+        user_power_total = user_power_total + np.array(user[temp_show_index].power)
+    plt.plot(range(len(user[0].date)), user_power_total, 'b')
+    plt.plot(range(len(user[0].power),len(user[0].power) + 30), power_2016_9_total, 'r')
+    plt.show()
 
 def write_csv(csv_name, predict_power):
     print('--export date')
@@ -115,7 +135,8 @@ def write_csv(csv_name, predict_power):
         writer = csv.writer(f)
         writer.writerow(['predict_date', 'predict_power_consumption'])
         for date in range(30):
-            writer.writerow([20160901+date, int(predict_power[date])])
+            writer.writerow([20160901 + date, int(predict_power[date])])
+
 
 if __name__ == '__main__':
     # import date
@@ -127,15 +148,20 @@ if __name__ == '__main__':
     # show average power
     # show_figure(range(0,len(user)), user, sum = True)
     # train and predict
+    average_error_vaild_total = 0
     power_2016_9_total = np.zeros(30)
     for i in range(len(user)):
-        user[i].train()
-        # user[i].vaild()
-        power_2016_9_total = power_2016_9_total + np.array(user[i].predict())
+        # train for vaild
+        user[i].train(0, 578)
+        user[i].vaild(578, 609)
+        average_error_vaild_total = average_error_vaild_total + user[i].average_error_vaild
+        # train for predict
+        user[i].train(0, 609)
+        power_2016_9_total = power_2016_9_total + np.array(user[i].predict(609, 639))
+    print('average_error_vaild: ' + str(average_error_vaild_total))
+    print('power_2016_9_total:')
     print(power_2016_9_total)
+    # show predict result
+    show_result(user, power_2016_9_total)
     # export
     write_csv('Tianchi_power_predict_table.csv', power_2016_9_total)
-
-
-
-
