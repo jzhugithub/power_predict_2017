dda26bf0be2336f865ab2aaadee19689f2e404e0
jzhugithub <mfx106825372@163.com>
2017-05-25 14:58:56 +0800

    update the structure of train, vaild and predict

--- run.py
+++ run.py
@@ -3,14 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def avg(list):
+    return sum(list) / len(list)
+
+
 class User(object):
     # information
     id = 'id'
     date = []
     power = []
-    # model data
-    average_power = 0.0
-    delta_power_in_week = np.zeros(7)
+    # model
+    pre_average_length_best = 90
+    delta_in_week_length_best = 0
 
     def __init__(self, userid):
         self.id = userid
@@ -21,28 +25,55 @@ class User(object):
         self.date.append(a_date)
         self.power.append(int(a_power))
 
-    def train(self, array_begin, array_end):
-        self.average_power = self.get_average_power(array_end - 20, array_end)
-        # print('average_power ' + self.id + ': ' + str(self.average_power))
-        self.delta_power_in_week = self.get_delta_power_in_week(array_end - 395, array_end - 335)
-        # print(self.delta_power_in_week)
+    def train(self, arrays):
+        pre_average_length_min = range(len(arrays))
+        delta_in_week_length = range(len(arrays))
+        for i in range(len(arrays)):
+            model_error_min = 1000000000
+            for pal in range(10, 100, 10):
+                for diwl in range(10, 150, 10):
+                    model_error_temp = self.model_error(arrays[i], [pal, diwl])
+                    if model_error_temp < model_error_min:
+                        model_error_min = model_error_temp
+                        pre_average_length_min[i] = pal
+                        delta_in_week_length[i] = diwl
+        self.pre_average_length_best = avg(pre_average_length_min)
+        self.delta_in_week_length_best = avg(delta_in_week_length)
 
-    def predict(self, array_begin, array_end):
+    def model_error(self, array, parameter):
+        power_predict = self.model(array[0], array[1], parameter)
+        error = np.array(self.power[array[0]:array[1]]) - np.array(power_predict)
+        return avg(abs(error))
+
+    def model(self, array_begin, array_end, parameter):
+        pre_average_length = parameter[0]
+        delta_in_week_length = parameter[1]
+        # average: parameter: pre_average_length
+        pre_average_last = self.get_average_power(array_begin - 365 - pre_average_length, array_begin - 365)
+        pre_average = self.get_average_power(array_begin - pre_average_length, array_begin)
+        pre_average_delta = pre_average - pre_average_last
+        average = self.get_average_power(array_begin - 365, array_end) + pre_average_delta
+        # delta_in_week: parameter: delta_in_week_length
+        delta_in_week = self.get_delta_in_week(array_begin - delta_in_week_length, array_begin)
+        # generate power_predict
         power_predict = []
         for i, value in enumerate(range(array_begin, array_end)):
-            power_predict.append(self.average_power + self.delta_power_in_week[value % 7])
+            power_predict.append(average + delta_in_week[value % 7])
         return power_predict
 
-    def vaild(self, array_begin, array_end):
-        power_predict = self.predict(array_begin, array_end)
-        error_vaild = np.array(self.power[array_begin:array_end]) - np.array(power_predict)
-        self.average_error_vaild = sum(abs(error_vaild)) / float(len(error_vaild))
-        # print('average_error_vaild ' + self.id + ': ' + str(self.average_error_vaild))
+    def evaluate(self, arrays):
+        errors = range(len(arrays))
+        for i in range(len(arrays)):
+            errors[i] = self.model_error(arrays[i], [self.pre_average_length_best, self.delta_in_week_length_best])
+        return avg(errors)
+
+    def predict(self, array_begin, array_end):
+        return self.model(array_begin, array_end, [self.pre_average_length_best, self.delta_in_week_length_best])
 
     def get_average_power(self, array_begin, array_end):
         return sum(self.power[array_begin:array_end]) / float(array_end - array_begin)
 
-    def get_delta_power_in_week(self, array_begin, array_end):
+    def get_delta_in_week(self, array_begin, array_end):
         power_in_week = np.zeros(7)
         average_power_in_week = np.zeros(7)
         date_count = np.zeros(7)
@@ -52,16 +83,13 @@ class User(object):
         for index in range(7):
             average_power_in_week[index] = power_in_week[index] / float(date_count[index])
         delta_power_in_week = average_power_in_week - average_power_in_week.sum() / 7.0
-
         return delta_power_in_week
 
 
 def import_date(csv_name):
     print('--import date')
-
     user = []
     user_id = []
-
     with open(csv_name, 'rb') as f:
         reader = csv.reader(f)
         temp_user = User('0')
@@ -104,6 +132,12 @@ def preprocessing(user):
                     user[i].date.insert(index, date)
                     add_power = (user[i].power[index - 1] + user[i].power[index]) / 2
                     user[i].power.insert(index, add_power)
+
+    print('-add predict 2016.9 terms')
+    for i in range(len(user)):
+        for date in range(30):
+            user[i].append('2016/9/' + str(date + 1), -1)
+
     return user
 
 
@@ -117,8 +151,8 @@ def merge_user(user):
     return user_total
 
 
-def show_figure(show_index, user, sum=False):
-    if sum:
+def show_figure(show_index, user, sum_flag=False):
+    if sum_flag:
         user_power_total = np.zeros(len(user[0].power))
         for temp_show_index in show_index:
             user_power_total = user_power_total + np.array(user[temp_show_index].power)
@@ -131,12 +165,9 @@ def show_figure(show_index, user, sum=False):
             plt.show()
 
 
-def show_result(user, power_2016_9_total):
-    user_power_total = np.zeros(len(user[0].power))
-    for temp_show_index in range(len(user)):
-        user_power_total = user_power_total + np.array(user[temp_show_index].power)
-    plt.plot(range(len(user[0].date)), user_power_total, 'b')
-    plt.plot(range(len(user[0].power), len(user[0].power) + 30), power_2016_9_total, 'r')
+def show_result(user_total):
+    plt.plot(range(len(user_total.date) - 30), user_total.power[:-30], 'b')
+    plt.plot(range(len(user_total.date) - 30, len(user_total.date)), user_total.power[-30:], 'r')
     plt.show()
 
 
@@ -162,30 +193,24 @@ if __name__ == '__main__':
     # show average power
     # show_figure(range(0,len(user)), user, sum = True)
 
-    # train and predict in total
-    user_total.train(0, 578)  # train for vaild
-    user_total.vaild(578, 609)
-    average_error_vaild_total = user_total.average_error_vaild
-    user_total.train(0, 609)  # train for predict
-    power_2016_9_total = user_total.predict(609, 639)
+    # train
+    train_arrays = [[519, 549], [549, 579], [579, 609]]
+    user_total.train(train_arrays)
+    error_train = user_total.evaluate(train_arrays)
+    print('error_train: ' + str(error_train))
 
-    # train and predict in  separateness
-    # average_error_vaild_total = 0
-    # power_2016_9_total = np.zeros(30)
-    # for i in range(len(user)):
-    #     # train for vaild
-    #     user[i].train(0, 578)
-    #     user[i].vaild(578, 609)
-    #     average_error_vaild_total = average_error_vaild_total + user[i].average_error_vaild
-    #     # train for predict
-    #     user[i].train(0, 609)
-    #     power_2016_9_total = power_2016_9_total + np.array(user[i].predict(609, 639))
-    print('average_error_vaild: ' + str(average_error_vaild_total))
-    print('power_2016_9_total:')
-    print(power_2016_9_total)
+    # vaild
+    vaild_arrays = [[534, 564], [564, 594]]
+    error_vaild = user_total.evaluate(vaild_arrays)
+    print('error_vaild ' + str(error_vaild))
+
+    # predict
+    power_2016_9_total = user_total.predict(609, 639)
+    user_total.power[-30:] = power_2016_9_total
+    print('power_2016_9_total: ' + str(power_2016_9_total))
 
     # show predict result
-    show_result(user, power_2016_9_total)
+    show_result(user_total)
 
     # export
     write_csv('Tianchi_power_predict_table.csv', power_2016_9_total)
