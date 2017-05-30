4c260ff69b7bfe4773df147e09d45f8c55710fba
jzhugithub <mfx106825372@163.com>
2017-05-30 15:01:46 +0800

    update

--- show_predict.py
+++ show_predict.py
@@ -12,9 +12,15 @@ def import_date(csv_name):
     return data
 
 
+d = import_date('Tianchi_power_predict_table.csv')
 d26 = import_date('Tianchi_power_predict_table_5.26.csv')
 d27 = import_date('Tianchi_power_predict_table_5.27.csv')
-date = range(len(d26))
-plt.plot(date, d26, 'k')
-plt.plot(date, d27, 'r')
+d28 = import_date('Tianchi_power_predict_table_5.28.csv')
+d29 = import_date('Tianchi_power_predict_table_5.29.csv')
+d30 = import_date('Tianchi_power_predict_table_5.30.csv')
+date = range(len(d))
+plt.plot(date, d, 'k')
+plt.plot(date, d28, 'r')
+plt.plot(date, d29, 'g')
+plt.plot(date, d30, 'b')
 plt.show()
